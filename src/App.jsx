5178ec770705293cd7e373f42b3cee1a5c743d23
deep-132 <deepmaheshwari23@gmail.com>
2025-11-12 17:18:04 +0530

    added necessary packages, tailwind and assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <div className="container-fluid">
-        <h1>Deepansh Lakhotia</h1>
-        <p>I am a software engineer</p>
+      <div className="container-fluid text-3xl text-center">
+        <h1 className="text-4xl font-bold">Deepansh Lakhotia</h1>
+        <p className="text-2xl font-bold">I am a software engineer</p>
       </div>
     </main>
   );
