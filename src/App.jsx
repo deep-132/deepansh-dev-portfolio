0e6917d63c8e00b788d73dee8a33c5a3d6ecc0fa
deep-132 <deepmaheshwari23@gmail.com>
2025-11-12 17:00:18 +0530

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="container-fluid">
+        <h1>Deepansh Lakhotia</h1>
+        <p>I am a software engineer</p>
+      </div>
+    </main>
+  );
+};
+
+export default App;
