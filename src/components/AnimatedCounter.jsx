17eb585ec1eba1d03fafa96e7a78b2db12465dbe
deepansh.lakhotia <deepansh.lakhotia@navikenz.com>
2025-11-16 18:18:55 +0530

    added react-count library to add count up till the counter values

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
+import CountUp from "react-countup";
 
 import { counterItems } from "../constants";
 
@@ -40,14 +41,13 @@ const AnimatedCounter = () => {
   return (
     <div id="counter" ref={counterRef} className="padding-x-lg xl:mt-0 mt-32">
       <div className="mx-auto grid-4-cols">
-        {counterItems.map((item, index) => (
-          <div
-            key={index}
-            ref={(el) => el && (countersRef.current[index] = el)}
-            className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center"
-          >
-            <div className="counter-number text-white-50 text-5xl font-bold mb-2">
-              0 {item.suffix}
+        {counterItems.map((item) => (
+          <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
+            <div
+              key={counterItems.label}
+              className="counter-number text-white-50 text-5xl font-bold mb-2"
+            >
+              <CountUp end={item.value} suffix={item.suffix} />
             </div>
             <div className="text-white-50 text-lg">{item.label}</div>
           </div>
