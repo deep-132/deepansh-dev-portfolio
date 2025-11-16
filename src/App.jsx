89259f415021f4bceaa5d5b7bb4b982b3789afcd
deep-132 <deepmaheshwari23@gmail.com>
2025-11-16 16:39:38 +0530

    implemented the hero section and imported 3d room model, and adjusted lights on the model

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import React from "react";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
-    <main>
-      <div className="container-fluid text-3xl text-center">
-        <h1 className="text-4xl font-bold">Deepansh Lakhotia</h1>
-        <p className="text-2xl font-bold">I am a software engineer</p>
-      </div>
-    </main>
+    <>
+      <Hero />
+    </>
   );
 };
 
